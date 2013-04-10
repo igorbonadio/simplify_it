4227f4233f62e0e07161960871f79c74e71b056a
Ígor Bonadio <igorbonadio@gmail.com>
2013-04-09 23:46:28 -0300

    refactoring negative's spec

--- spec/negative_spec.rb
+++ spec/negative_spec.rb
@@ -3,8 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 module SimplifyIt
   describe Negative do
     it "should set a number to negative" do
-      number = Negative.new(3)
-      number.eval.should be == -3
+      Negative.new(3).eval.should be == -3
     end
 
     it "should set a negative number to positve" do
@@ -12,18 +11,15 @@ module SimplifyIt
     end
 
     it "should set a negative number to positive" do
-      number = Negative.new(Negative.new(3))
-      number.eval.should be == 3
+      Negative.new(Negative.new(3)).eval.should be == 3
     end
 
     it "should set an expression to negative" do
-      expression = Negative.new(Addition.new(1, 2, 3, 4))
-      expression.eval.should be == -10
+      Negative.new(Addition.new(1, 2, 3, 4)).eval.should be == -10
     end
 
     it "should set an expression to negative" do
-      expression = Negative.new(Addition.new(1, Negative.new(2), 3, 4))
-      expression.eval.should be == -6
+      Negative.new(Addition.new(1, Negative.new(2), 3, 4)).eval.should be == -6
     end
 
     it "should have a string representation" do
