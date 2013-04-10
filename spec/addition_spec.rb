8e9ed8ceb81666221dae5aaa3c9f4c7c87e4fb62
Ígor Bonadio <igorbonadio@gmail.com>
2013-04-09 23:47:07 -0300

    refactoring positive's spec

--- spec/addition_spec.rb
+++ spec/addition_spec.rb
@@ -3,18 +3,15 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 module SimplifyIt
   describe Addition do
     it "should add numbers" do
-      addition = Addition.new(1,2,3,4)
-      addition.eval.should be == 10
+      Addition.new(1,2,3,4).eval.should be == 10
     end
 
     it "should add negative numbers" do
-      addition = Addition.new(Negative.new(1), Negative.new(2), Negative.new(3), Negative.new(4))
-      addition.eval.should be == -10
+      Addition.new(Negative.new(1), Negative.new(2), Negative.new(3), Negative.new(4)).eval.should be == -10
     end
 
     it "should add negative and positive numbers" do
-      addition = Addition.new(1, Negative.new(2), Negative.new(3), 4)
-      addition.eval.should be == 0
+      Addition.new(1, Negative.new(2), Negative.new(3), 4).eval.should be == 0
     end
 
     it "should have a string representation" do
