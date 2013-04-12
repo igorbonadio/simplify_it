5bda22f81a8cfaaedcbd75af103fa7d8359b5892
Ígor Bonadio <igorbonadio@gmail.com>
2013-04-12 10:38:33 -0300

    fixnum definitions in a single file

--- lib/simplify_it.rb
+++ lib/simplify_it.rb
@@ -1,5 +1,6 @@
 ROOT_PATH = File.expand_path(File.dirname(__FILE__))
 
+require File.join(ROOT_PATH, '/simplify_it/fixnum')
 require File.join(ROOT_PATH, '/simplify_it/negative')
 require File.join(ROOT_PATH, '/simplify_it/positive')
 require File.join(ROOT_PATH, '/simplify_it/addition')
---
+++ lib/simplify_it/fixnum.rb
@@ -0,0 +1,21 @@
+class Fixnum
+  def eval
+    self
+  end
+
+  def to_negative
+    -1 * self
+  end
+
+  def simplified?
+    true
+  end
+
+  def endpoint?
+    true
+  end
+
+  def next_step
+    raise Exception.new("It can not be simplified!")
+  end
+end
